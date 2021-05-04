a2a445793e70fbe06da55787fcce4af6082bfea2
RlZkhere <48204569+RlZkhere@users.noreply.github.com>
2021-05-04 12:07:10 +0200

    Update main.c

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <string.h>
 int main () {
 printf ("Answer the question for captcha security check\n");
-printf ("What's the number beetween 1000 and 20000 ?\n");
+printf ("What's the medium number beetween 1000 and 20000 ?\n");
 int captcha  ;
 scanf ("%d", &captcha);
 if (captcha != 1500){
